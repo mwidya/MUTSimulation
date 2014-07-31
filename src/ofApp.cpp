044417f46b29b7c4b200409a2f6733fa947e5b3c
Martin Widyanata <mwidya@gmail.com>
2014-07-31 17:19:13 +0200

    completed planes

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,9 +5,11 @@ float factor = 0.1f;
 // 1.0 = 1 meter
 
 float levelDistance = 3000*factor;
-float middleDistance = 4300*factor;
 float f0f1Distance = 2150*factor;
 float f3f4Distance = 8600*factor;
+float f4f5Distance = 4300*factor;
+float f5f6Distance = 11100*factor;
+float f8f9Distance = 6000*factor;
 float f0Long = 4300*factor;
 float f0Short = 1700*factor;
 float f1Long = 3000*factor;
@@ -39,33 +41,54 @@ void ofApp::setup(){
     ofAddListener(syphonServerDirectory.events.serverUpdated, this, &ofApp::serverUpdated);
     ofAddListener(syphonServerDirectory.events.serverRetired, this, &ofApp::serverRetired);
     
-    easyCam.setDistance(20000*factor);
+    easyCam.setDistance(30000*factor);
     
     f0.set(f0Long, f0Short);
-    f0.setPosition(middleDistance*.5 + f3f4Distance + f2Long + f0f1Distance, 0, 0);
+    f0.setPosition(f4f5Distance*.5 + f3f4Distance + f2Long + f0f1Distance, 0, 0);
     f0.rotate(90, 0, 1, 0);
     
     f1.set(f1Long, f1Short);
-    f1.setPosition(middleDistance*.5 + f3f4Distance + f2Long, levelDistance + f1Short*.5, 0);
+    f1.setPosition(f4f5Distance*.5 + f3f4Distance + f2Long, levelDistance + f1Short*.5, 0);
     f1.rotate(90, 0, 1, 0);
     f1.rotate(180, 1, 0, 0);
     
     f2.set(f2Long, f2Short);
-    f2.setPosition(middleDistance*.5 + f3f4Distance + f2Long *.5, levelDistance, 0);
+    f2.setPosition(f4f5Distance*.5 + f3f4Distance + f2Long *.5, levelDistance, 0);
     f2.rotate(90, 1, 0, 0);
     
     f3.set(f3Long, f3Short);
-    f3.setPosition(middleDistance*.5 + f3f4Distance, levelDistance + f3Short*.5, 0);
+    f3.setPosition(f4f5Distance*.5 + f3f4Distance, levelDistance + f3Short*.5, 0);
     f3.rotate(90, 0, 1, 0);
     
     f4.set(f4Long, f4Short);
-    f4.setPosition(-(middleDistance * 0.5), 0, 0);
+    f4.setPosition(-(f4f5Distance * 0.5), 0, 0);
     f4.rotate(90, 0, 1, 0);
     f4.rotate(180, 1, 0, 0);
     
     f5.set(f5Long, f5Short);
-    f5.setPosition(middleDistance * 0.5, 0, 0);
+    f5.setPosition(f4f5Distance * 0.5, 0, 0);
     f5.rotate(90, 0, 1, 0);
+    
+    f6.set(f6Long, f6Short);
+    f6.setPosition(-(f4f5Distance*.5 + f5f6Distance), levelDistance + f5Short*.5, 0);
+    f6.rotate(90, 0, 1, 0);
+    f6.rotate(180, 1, 0, 0);
+    
+    f7.set(f7Long, f7Short);
+    f7.setPosition(-(f4f5Distance*.5 + f5f6Distance + f7Short*.5), levelDistance, 0);
+    f7.rotate(90, 1, 0, 0);
+    f7.rotate(90, 0, 1, 0);
+    
+    f8.set(f8Long, f8Short);
+    f8.setPosition(-(f4f5Distance*.5 + f5f6Distance + f7Short), levelDistance + f5Short*.5, 0);
+    f8.rotate(90, 0, 1, 0);
+    
+    f9.set(f8Long, f8Short);
+    f9.setPosition(-(f4f5Distance*.5 + f5f6Distance + f7Short + f8f9Distance), 0, 0);
+    f9.rotate(90, 0, 1, 0);
+    
+    
+    
     
     planes.push_back(&f0);
     planes.push_back(&f1);
@@ -73,6 +96,10 @@ void ofApp::setup(){
     planes.push_back(&f3);
     planes.push_back(&f4);
     planes.push_back(&f5);
+    planes.push_back(&f6);
+    planes.push_back(&f7);
+    planes.push_back(&f8);
+    planes.push_back(&f9);
     
     for (int i = 0; i < planes.size(); i++) {
         mutPlane *plane = planes[i];
